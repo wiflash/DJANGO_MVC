cc0325fd6a05f94b89f27765d4b085cebc297bc2
Wildan Firdaus <firdaus@alterra.id>
2019-12-09 10:51:06 +0700

    Added virtual environment and django projects in each folders

---
+++ A/env_project_model/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/alta-18/Documents/Alterra/DJANGO_MVC/A/env_project_model/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ B/env_project_views/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/alta-18/Documents/Alterra/DJANGO_MVC/B/env_project_views/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
