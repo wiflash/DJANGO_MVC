46277e3d866a65e1b0790aa83bd62cd2639d14f4
Wildan Firdaus <firdaus@alterra.id>
2019-12-10 23:32:55 +0700

    Added MVC Challenge-4

---
+++ C/project_advance_views/alpha_tech/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+# Create your models here.
+class Blog(models.Model):
+    picture = models.CharField(max_length=200, default="")
+    title = models.CharField(max_length=100, default="")
+    date = models.DateField(auto_now_add=True)
+    content = models.TextField(default="")
+
+    def __str__(self):
+        return self.title
+
+class Mentor(models.Model):
+    profile_pic = models.CharField(max_length=200, default="")
+    name = models.CharField(max_length=50, default="")
+    experience = models.CharField(max_length=50, default="")
+    year_exp = models.IntegerField(default=0)
+    quotes = models.CharField(max_length=200, default="")
+
+    def __str__(self):
+        return self.name
+
+class Mentee(models.Model):
+    profile_pic = models.CharField(max_length=200, default="")
+    name = models.CharField(max_length=50, default="")
+    quotes = models.CharField(max_length=200, default="")
+
+    def __str__(self):
+        return self.name
---
+++ C/project_advance_views/alpha_tech/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render, redirect
+from .models import *
+
+# Create your views here.
+def home(request):
+    return render(request, 'alpha_tech/home.html', {})
+
+def blog(request):
+    fields = {"posts": Blog.objects.all()}
+    return render(request, 'alpha_tech/blog.html', fields)
+
+def mentor(request):
+    fields = {"mentors": Mentor.objects.all()}
+    return render(request, 'alpha_tech/mentor.html', fields)
+
+def mentee(request):
+    fields = {"mentees": Mentee.objects.all()}
+    return render(request, 'alpha_tech/mentee.html', fields)
+
+def author(request):
+    return render(request, 'alpha_tech/author.html', {})
+
+def post(request, blog_id):
+    fields = {"post": Blog.objects.get(pk=blog_id)}
+    return render(request, 'alpha_tech/blog-post.html', fields)
+
+def form(request):
+    return render(request, 'alpha_tech/blog-form.html', {})
+
+def submit(request):
+    post = Blog(
+        picture = request.POST['picture'],
+        title = request.POST['title'],
+        content = request.POST['content']
+    )
+    post.save()
+    return redirect('blog/')
