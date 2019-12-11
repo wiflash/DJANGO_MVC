b29361f3b7d841ea0f942a732a12fcbe606a1514
Wildan Firdaus <firdaus@alterra.id>
2019-12-11 11:00:56 +0700

    modified pages

--- alpha_tech/views.py
+++ alpha_tech/views.py
@@ -3,28 +3,31 @@ from .models import *
 
 # Create your views here.
 def home(request):
-    return render(request, 'alpha_tech/home.html', {})
+    fields = {"flag": 1}
+    return render(request, 'alpha_tech/home.html', fields)
 
 def blog(request):
-    fields = {"posts": Blog.objects.all()[::-1]}
+    fields = {"posts": Blog.objects.all()[::-1], "flag": 1}
     return render(request, 'alpha_tech/blog.html', fields)
 
 def mentor(request):
-    fields = {"mentors": Mentor.objects.all()}
+    fields = {"mentors": Mentor.objects.all(), "flag": 1}
     return render(request, 'alpha_tech/mentor.html', fields)
 
 def mentee(request):
-    fields = {"mentees": Mentee.objects.all()}
+    fields = {"mentees": Mentee.objects.all(), "flag": 1}
     return render(request, 'alpha_tech/mentee.html', fields)
 
 def author(request):
-    return render(request, 'alpha_tech/author.html', {})
+    fields = {"flag": 1}
+    return render(request, 'alpha_tech/author.html', fields)
 
 def post(request, blog_id):
-    fields = {"post": Blog.objects.get(pk=blog_id)}
+    fields = {"post": Blog.objects.get(pk=blog_id), "flag": 1}
     return render(request, 'alpha_tech/blog-post.html', fields)
 
 def form(request):
+    fields = {"flag": 1}
     return render(request, 'alpha_tech/blog-form.html', {})
 
 def submit(request):
